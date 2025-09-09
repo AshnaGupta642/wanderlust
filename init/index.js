2ccf0774854093be87a693af959f1dd5dcd6502b
Ashna Gupta <ashnagupta9a03@gmail.com>
2025-09-09 18:40:05 +0530

    changes

--- init/index.js
+++ init/index.js
@@ -1,8 +1,12 @@
+require("dotenv").config({ path: "../.env" });
+
+
 const mongoose=require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const mongo_url ="mongodb://127.0.0.1:27017/wanderlust";
+const mongo_url =process.env.ATLASDB_URL;
+
 main ()
 .then(()=>{
     console.log("connected to db");
